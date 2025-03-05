3167bae7d3b006680fdd5ecac6c7338594962f39
wendy nixon <wendykaenixon@gmail.com>
2025-03-05 14:29:05 -0600

    finished code

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write ("What is your last name? ");
+        string last= Console.ReadLine();
+
+        Console.Write ($"Your name is {last}, {first} {last}");
+
+
+
     }
 }
