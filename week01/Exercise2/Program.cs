37b21879656db5dd3b9e21cd647d8fbb63a72890
wendy nixon <wendykaenixon@gmail.com>
2025-03-06 13:22:57 -0600

    did it

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Please give my your grade percentage:");
+        int score = int.Parse(Console.ReadLine());
+        
+        string letter;
+        string sign= "";
+        int lastDigit= score % 10;
+
+            if (score >= 90)
+            {
+                letter = "A";
+            
+            }
+            else if (score >= 80)
+            {
+                letter = "B";
+            }
+            else if (score >= 70)
+            {
+                letter= "C";
+            }
+            else if (score>= 60)
+            {
+                letter = "D";
+            }
+            else 
+            {
+                letter = "F";
+            }
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit<=3)
+            {
+                sign = "-";
+            }
+
+            Console.WriteLine ($"You got a {letter}{sign}");
+            
+            if (score>70)
+            {
+                Console.WriteLine ("You passed");
+            }
+            else
+            {
+                Console.WriteLine ("Sorry you didn't pass");
+            }
     }
 }
