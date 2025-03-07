76b683e1c772458fba113bfd3ea1332e23ca2e4a
wendy nixon <wendykaenixon@gmail.com>
2025-03-07 12:18:55 -0600

    finished project

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Reflection.Emit;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+
+        
+
+        int guess = -1;
+        while (guess != number)
+        {
+            Console.WriteLine ("What is your guess?:");
+            guess = int.Parse(Console.ReadLine());
+        
+
+            if (number==guess)
+            {
+                Console.WriteLine($"The number was {number}, You guessed it. Good job");
+            }
+            else if  (number< guess)
+            {
+                Console.WriteLine($"Your guess of {guess}, is too high. Guess a lower number: ");
+            }
+            else
+            {
+                Console.WriteLine($"Your guess of {guess} is to low. Guess a higher number:");
+            }
+        }
+
     }
 }
