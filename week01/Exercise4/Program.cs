2f506a6e9213cdd9575b0a128192731b7c135204
wendy nixon <wendykaenixon@gmail.com>
2025-03-08 10:59:17 -0600

    finished it

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,73 @@
 using System;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+       List<int> numbers= new List<int>();
+        
+ 
+        Console.WriteLine("Please give me a series of numbers. Enter 0 to end it:");
+        
+
+        int userInput = -1;
+     
+        while (userInput != 0)
+        {
+            Console.WriteLine ("Enter a number: ");
+            userInput = int.Parse(Console.ReadLine());
+            if (userInput != 0)
+            {
+                numbers.Add(userInput);
+            }
+        }
+
+        int totalSum = 0;
+        foreach (int num in numbers)
+        {
+            totalSum+= num;
+            
+
+        }
+        Console.WriteLine ($"The total is {totalSum}"); 
+        
+        if (numbers.Count>0)  
+        {
+            double average = (double)totalSum / numbers.Count;
+            Console.WriteLine ($"The average is {average}");
+        }
+        else
+        {
+            Console.WriteLine ("Sorry, no numbers");
+        }
+
+        int largest = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num > largest)
+            {
+                largest = num;
+            }
+        }
+        Console.WriteLine ($"The largest number is {largest}");
+
+        
+        
+
+        
+                
+         
+            
+           
+
+        
     }
+
+    
+    
+        
+    
 }
